4724a22a9243666f39976b23a6fe090599ba502d
hafeezhmha <hafeezhmha50@gmail.com>
2025-12-27 20:59:29 +0530

    fixed routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/api/ingest',
+          destination: '/api/ingest.py',
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
